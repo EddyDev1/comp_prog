5d80e973a7aa07dcc456364f9eeb98cd427965c2
Edward Asante <edwardasante@my.unt.edu>
2023-11-25 17:50:34 -0600

    correcting setup

---
+++ CSES/Weird_Algorithm.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void run_case(){
+  return;
+}
+
+int main(){
+  int tests;
+  cin >> tests;
+
+  while(tests--){
+    run_case();
+  }
+  
+  return 0;
+}
