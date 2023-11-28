4aa7c370a24274b61ff1145d7258777f93dd0084
Edward Asante <edwardasante@my.unt.edu>
2023-11-27 20:21:06 -0600

    Dont know how to approach, come back later

---
+++ CSES/Increasing_Array.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void run_case(){
+  return;
+}
+
+int main(){
+  int N;
+  cin >> N;
+
+  vector<int> n(N, 0);
+  int num;
+
+  for(auto& x : n)
+    cin>>num;
+
+  return 0;
+}
