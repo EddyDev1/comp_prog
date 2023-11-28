4a272ced60ab7f8ffa3f80627c8a005aef93904f
Edward Asante <edwardasante@my.unt.edu>
2023-11-27 20:37:20 -0600

    Cant submit old problems, doubt this works

---
+++ Codeforces/Watermelon.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void run_case(){
+  return;
+}
+
+int main(){
+  int n;
+  cin >> n;
+
+  if(n % 2 == 0){
+    cout<<"YES";
+  } else {
+    cout<<"NO";
+  }
+
+  return 0;
+}
