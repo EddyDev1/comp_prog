ac844403747ff228fbc490a776554eaf1a4d432c
Edward Asante <edwardasante@my.unt.edu>
2023-12-02 19:21:50 -0600

    added generic debugging template

--- CSES/template.cpp
+++ CSES/template.cpp
@@ -17,11 +17,26 @@
 
 using namespace std;
 
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+#ifdef NEAL_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
 void run_case(){
   return;
 }
 
-int main(){
+int main() {
+  ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+  cin.tie(nullptr);
+#endif
   int tests;
   cin >> tests;
 
