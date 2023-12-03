e0d0226a02eb93045e6fd83ea8d6270a298cf53d
Edward Asante <edwardasante@my.unt.edu>
2023-12-02 19:19:43 -0600

    added debugger mosts ytbers use

--- Codeforces/template.cpp
+++ Codeforces/template.cpp
@@ -17,11 +17,26 @@
 
 using namespace std;
 
+void dbg_out() { std::cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+  std::cerr << ' ' << H;
+  dbg_out(T...);
+}
+#ifdef NEAL_DEBUG
+#define dbg(...) std::cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
 void run_case(){
   return;
 }
 
-int main(){
+int main() {
+  std::ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+  std::cin.tie(nullptr);
+#endif
   int tests;
   cin >> tests;
 
